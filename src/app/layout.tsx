55441b7313c8aa7010e3a0b401693f5dfea3a313
Surbee <surbee001@github.com>
2025-10-02 12:08:02 +0400

    fix: add explicit @font-face declaration for ABC Normal Book to ensure production loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 ﻿import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,8 +16,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://db.onlinewebfonts.com" />
-        <link href="https://db.onlinewebfonts.com/c/bad5d85dec62adc89f2f88300b74dd63?family=ABC+Normal+Book" rel="stylesheet" />
+        <link rel="preconnect" href="https://db.onlinewebfonts.com" crossOrigin="anonymous" />
+        <link 
+          href="https://db.onlinewebfonts.com/c/bad5d85dec62adc89f2f88300b74dd63?family=ABC+Normal+Book" 
+          rel="stylesheet" 
+          type="text/css"
+        />
+        <style dangerouslySetInnerHTML={{
+          __html: `
+            @font-face {
+              font-family: 'ABC Normal Book';
+              src: url('https://db.onlinewebfonts.com/t/bad5d85dec62adc89f2f88300b74dd63.eot');
+              src: url('https://db.onlinewebfonts.com/t/bad5d85dec62adc89f2f88300b74dd63.eot?#iefix') format('embedded-opentype'),
+                   url('https://db.onlinewebfonts.com/t/bad5d85dec62adc89f2f88300b74dd63.woff2') format('woff2'),
+                   url('https://db.onlinewebfonts.com/t/bad5d85dec62adc89f2f88300b74dd63.woff') format('woff'),
+                   url('https://db.onlinewebfonts.com/t/bad5d85dec62adc89f2f88300b74dd63.ttf') format('truetype'),
+                   url('https://db.onlinewebfonts.com/t/bad5d85dec62adc89f2f88300b74dd63.svg#ABC Normal Book') format('svg');
+              font-display: swap;
+            }
+          `
+        }} />
       </head>
       <body className="antialiased">{children}</body>
     </html>
